3176512bb175913dde8998767b589bda7fd853b9
jean trujillo <jtrujillo@acl.cl>
2022-03-10 21:47:32 -0400

    add: Evaluation, test get all evaluations

--- src/test/java/io/getarrays/userservice/controller/EvaluationTest.java
+++ src/test/java/io/getarrays/userservice/controller/EvaluationTest.java
@@ -1,4 +1,5 @@
 package io.getarrays.userservice.controller;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
@@ -17,6 +18,13 @@ import io.getarrays.userservice.repository.entity.Evaluation;
 import io.getarrays.userservice.service.implementation.EvaluationServiceImplementation;
 import io.getarrays.userservice.service.implementation.ProfileServiceImplementation;
 
+
+
+/**
+ * @author JeanTrujillo
+ * @version 1.2
+ * @since 08/03/2022 09/03/2022 10/03/2022
+ */
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class EvaluationTest {
@@ -32,12 +40,12 @@ class EvaluationTest {
 	
 	
 	/**
-	 * Pre-condiciones para ejecutar los TEST.
-	 * Mockito de la capa servicio.
+	 * Preconditions to execute the TEST.
+	 * Mock-up of the service layer.
 	 */
 	@BeforeEach
 	void setUp() {
-		/*SaveEvaluation*/
+		/*Save a Evaluation*/
 		Evaluation eval = mockEvaluation();
 		EvaluationDTO evaluationDTO = new EvaluationDTO();
 		evaluationDTO.setDate(eval.getDate());
@@ -47,8 +55,17 @@ class EvaluationTest {
 		evaluationDTO.setProfileId( (long) 9 );
 		Mockito.when(evaluationService.save( evaluationDTO )).thenReturn( mockEvaluation() ); //Mockito.any()
 		
+		/*Get All Evaluations*/
+		List<Evaluation> listEvaluation = new ArrayList<>();
+		listEvaluation.add( mockEvaluation() );
+		listEvaluation.add( mockEvaluation() );
+		listEvaluation.add( mockEvaluation() );
+		Mockito.when(evaluationService.findAll()).thenReturn( listEvaluation ); 
 	}
 
+	/**
+	 * TEST Create a new Evaluation
+	 */
 	@Test
 	void testSaveEvaluation() {
 		EvaluationDTO evaluationDTO = new EvaluationDTO();
@@ -58,30 +75,26 @@ class EvaluationTest {
 		evaluationDTO.setScore(5);
 		evaluationDTO.setProfileId((long) 9);
 
-		
 		ResponseEntity<Evaluation> actualValue = evaluationController.save(evaluationDTO);
 			//Evaluation expectedValue = new Evaluation(null, "06/03/2022", "Title", "Observation", 5);
 			//Assertions.assertEquals(actualValue, expectedValue);
-
 		Assertions.assertNotNull(actualValue);
 	}
 
+	/**
+	 * Get All Evaluations
+	 */
 	@Test
 	void testGetAllEvaluation() {
 		List<Evaluation> expectedValue = evaluationController.getEvaluations();
 		Assertions.assertEquals(3, expectedValue.size());
 	}
 
-	private EvaluationDTO mockAddEvaluationDTO() {
-		EvaluationDTO evaluationDTO = new EvaluationDTO();
-		evaluationDTO.setDate("06/03/2022");
-		evaluationDTO.setObservation("Observation");
-		evaluationDTO.setTitle("Title");
-		evaluationDTO.setScore(5);
-		evaluationDTO.setProfileId((long) 9);
-		return evaluationDTO;
-	}
-	
+
+	/**
+	 * Method dummy test object
+	 * @return Evaluation mock
+	 */
 	private Evaluation mockEvaluation() {
 		Evaluation evaluation = new Evaluation();
 		evaluation.setDate("06/03/2022");
